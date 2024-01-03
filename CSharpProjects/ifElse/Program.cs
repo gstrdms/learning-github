bd55ab8cb680dfdc95ee60835a1821805b17d441
gstrdms <hrynzm@outlook.com>
2024-01-03 00:31:29 -0500

    added booleans

--- CSharpProjects/ifElse/Program.cs
+++ CSharpProjects/ifElse/Program.cs
@@ -11,7 +11,25 @@ int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-if (total > 14)
+//Add another IF statement to implement the doubles bonus
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
+
+//Add another IF statement to implement the triples bonus
+
+if ((roll1 == roll2) && (roll2 == roll3)) 
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+}
+
+//Add an IF statement to display different messages based on the value of the total variable
+
+if (total => 15)
 {
         Console.WriteLine("You win!");
 }
@@ -20,3 +38,4 @@ if (total < 15)
 {
     Console.WriteLine("sorry, you lose.");
 }
+
