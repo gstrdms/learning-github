e7c46a208adb383977708c82474d956d0e471931
gstrdms <hrynzm@outlook.com>
2024-01-03 00:10:02 -0500

    2

--- CSharpProjects/ifElse/obj/Debug/net8.0/ifElse.AssemblyInfo.cs
+++ CSharpProjects/ifElse/obj/Debug/net8.0/ifElse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ifElse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2490d286ad9f62bc80055011a2378838346075c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ifElse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ifElse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
